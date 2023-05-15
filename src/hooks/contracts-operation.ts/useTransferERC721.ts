012d9a02dbad1f4f8d79fc0cecf64b2aa69ac0eb
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-15 17:24:53 +0700

    chore: send name

---
+++ src/hooks/contracts-operation.ts/useTransferERC721.ts
@@ -0,0 +1,56 @@
+import { ContractOperationHook, TransactionType, EventType } from '@/interfaces/contract-operation';
+import { useCallback } from 'react';
+import { useUserSecretKey } from '@/state/wallet/hooks';
+import WError, { ERROR_CODE } from '@/utils/error';
+import { getContractSigner } from '@/utils/contract.signer';
+import useProvider from '@/hooks/useProvider';
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import { TRANSFER_TX_SIZE } from '@/configs';
+import ERC721ABIJson from '@/abis/erc721.json';
+
+export interface ITransferERC721 {
+  receiver: string;
+  tokenID: string;
+  tokenAddress: string;
+}
+
+const useTransferERC721: ContractOperationHook<ITransferERC721, TransactionResponse> = () => {
+  const userSecretKey = useUserSecretKey();
+  const provider = useProvider();
+
+  const estimateGas = useCallback(
+    async (params: ITransferERC721) => {
+      if (!userSecretKey?.privateKey || !provider) {
+        throw new WError(ERROR_CODE.ACCOUNT_EMPTY);
+      }
+      const contract = getContractSigner(params.tokenAddress, ERC721ABIJson.abi, provider, userSecretKey.privateKey);
+      const gasLimit = await contract.estimateGas.transferFrom(userSecretKey.address, params.receiver, params.tokenID);
+      return gasLimit.toNumber();
+    },
+    [userSecretKey, provider],
+  );
+
+  const call = useCallback(
+    async (params: ITransferERC721): Promise<TransactionResponse> => {
+      if (!userSecretKey?.privateKey || !provider) {
+        throw new WError(ERROR_CODE.ACCOUNT_EMPTY);
+      }
+      const privateKey = userSecretKey.privateKey;
+      const { tokenAddress, receiver, tokenID } = params;
+      const contract = getContractSigner(tokenAddress, ERC721ABIJson.abi, provider, privateKey);
+      const tx: TransactionResponse = await contract.transferFrom(userSecretKey.address, receiver, tokenID);
+      return tx;
+    },
+    [userSecretKey, provider],
+  );
+
+  return {
+    call: call,
+    estimateGas,
+    transactionType: TransactionType.ERC721,
+    eventType: EventType.TRANSFER,
+    txSize: TRANSFER_TX_SIZE,
+  };
+};
+
+export default useTransferERC721;
