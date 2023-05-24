7621788474cfa6b32bd063665fdfb745e853b72f
2712 <2712@trustless.computer>
2023-05-24 14:52:02 +0700

    fix: update contract address to network

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,10 +1,10 @@
 import network from '@/lib/network.helpers';
 
 const ENVS = import.meta.env;
-
+const selectedNetwork = network.getSelectedNetwork();
 // App configs
 export const APP_ENV: string = ENVS.VITE_MODE;
-export const API_URL: string = network.getSelectedNetwork().BE_API;
+export const API_URL: string = selectedNetwork.BE_API;
 export const API_BLOCKSTREAM: string = ENVS.VITE_BLOCKSTREAM;
 export const CDN_URL: string = ENVS.VITE_CDN_URL;
 export const MOCKUP_PASSWORD: string = ENVS.VITE_MOCKUP_PASSWORD || '';
@@ -15,9 +15,9 @@ export const BTC_ADDRESS_TEST: string = ENVS.VITE_BTC_ADDRESS_TEST || '';
 export const CDN_URL_ICONS: string = CDN_URL + '/wallet-icons';
 
 // Contract configs
-export const ARTIFACT_CONTRACT: string = ENVS.VITE_ARTIFACT_CONTRACT;
-export const BNS_CONTRACT: string = ENVS.VITE_BNS_CONTRACT;
-export const BFS_ADDRESS: string = ENVS.VITE_BFS_CONTRACT;
+export const ARTIFACT_CONTRACT: string = selectedNetwork.ARTIFACT_CONTRACT;
+export const BNS_CONTRACT: string = selectedNetwork.BNS_CONTRACT;
+export const BFS_ADDRESS: string = selectedNetwork.BFS_CONTRACT;
 
 export const TRANSFER_TX_SIZE = 1000;
 export const DEPLOY_CONTRACT_TX_SIZE = 24000;
