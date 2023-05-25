9a11954e624cb8bb95b6789373caa7d27ac83c2b
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-25 17:23:17 +0700

    chore: update network via sdk

--- src/lib/network.helpers.ts
+++ src/lib/network.helpers.ts
@@ -1,60 +1,13 @@
 import storageLocal from '@/lib/storage.local';
 import { LocalStorageKey } from '@/enums/storage.keys';
 import { compareString } from '@/utils';
+import * as TC_CONNECT from 'tc-connect';
+
+export interface INetwork extends TC_CONNECT.INetwork {}
+export type IBTCNetwork = TC_CONNECT.IBTCNetwork;
 
 const ENVS = import.meta.env;
 const DEFAULT_NETWORK_KEY: string = ENVS.VITE_DEFAULT_NETWORK_KEY;
-
-export type IBTCNetwork = 'mainnet' | 'testnet' | 'regtest';
-
-export interface INetwork {
-  Name: string;
-  Key: string;
-  ChainID: number | string;
-  TCNode: string;
-  BTCNetwork: IBTCNetwork;
-  Explorer: string;
-  BE_API: string;
-  Icon: string;
-  BTCExplorer: string;
-  ConnectURL: string;
-  ARTIFACT_CONTRACT: string;
-  BNS_CONTRACT: string;
-  BFS_CONTRACT: string;
-}
-
-const NETWORKS: Array<INetwork> = [
-  {
-    Name: 'Mainnet',
-    BTCNetwork: 'mainnet',
-    ChainID: 22213,
-    TCNode: 'https://tc-node.trustless.computer',
-    Explorer: 'https://explorer.trustless.computer',
-    BE_API: 'https://dapp.trustless.computer/dapp/api',
-    Icon: 'ic-penguin-currency-dark.svg',
-    BTCExplorer: 'https://mempool.space',
-    Key: 'Mainnet',
-    ConnectURL: 'https://wadary.regtest.trustless.computer/relayer',
-    ARTIFACT_CONTRACT: '0x16EfDc6D3F977E39DAc0Eb0E123FefFeD4320Bc0',
-    BNS_CONTRACT: '0x8b46F89BBA2B1c1f9eE196F43939476E79579798',
-    BFS_CONTRACT: '0x8BAA6365028894153DEC048E4F4e5e6D2cE99C58',
-  },
-  {
-    Name: 'Regtest manual',
-    ChainID: 22213,
-    TCNode: 'https://tc-node-manual.regtest.trustless.computer',
-    BTCNetwork: 'regtest',
-    Explorer: 'https://explorer.regtest.trustless.computer',
-    BE_API: 'https://dapp.dev.trustless.computer/dapp/api',
-    Icon: 'ic-penguin-regtest-dark.svg',
-    BTCExplorer: 'https://blockstream.regtest.trustless.computer/regtest',
-    Key: 'Regtest manual',
-    ConnectURL: 'https://wadary.regtest.trustless.computer/relayer',
-    ARTIFACT_CONTRACT: '0x16EfDc6D3F977E39DAc0Eb0E123FefFeD4320Bc0',
-    BNS_CONTRACT: '0x8b46F89BBA2B1c1f9eE196F43939476E79579798',
-    BFS_CONTRACT: '0x8BAA6365028894153DEC048E4F4e5e6D2cE99C58',
-  },
-];
 
 const ADVANCE_NETWORKS: Array<INetwork> = [
   {
@@ -105,7 +58,7 @@ class Network {
 
   getListNetworks(): INetwork[] {
     const advanceNetworks = storageLocal.get(LocalStorageKey.ADVANCE_USER) ? ADVANCE_NETWORKS : [];
-    return [...NETWORKS, ...advanceNetworks];
+    return [...TC_CONNECT.NETWORKS, ...advanceNetworks];
   }
 
   switchNetwork(network: INetwork) {
