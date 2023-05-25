6eae63ac495d86f313a3b9b44a4d046ec7a473f6
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-25 18:03:28 +0700

    chore: update signmessage

--- src/components/Transactor/DappRequest/RequestAccount.modal.tsx
+++ src/components/Transactor/DappRequest/RequestAccount.modal.tsx
@@ -1,13 +1,16 @@
-import BaseModal from '@/components/BaseModal';
 import React from 'react';
-import { Container, ButtonGroup } from './styled';
+import { Container, ButtonGroup, AdvanceWrapper, Divider } from './styled';
 import * as TC_CONNECT from 'tc-connect';
-import { useCurrentUserInfo } from '@/state/wallet/hooks';
+import { useCurrentUserInfo, useUserSecretKey } from '@/state/wallet/hooks';
 import Text from '@/components/Text';
 import Button from '@/components/Button';
 import { useAppSelector } from '@/state/hooks';
 import { listAccountsSelector } from '@/state/wallet/selector';
 import { getConnector } from '@/lib/connector.helper';
+import SelectAccount from '@/components/SelectAccount';
+import SignerModal from '@/components/SignerModal';
+import { getWalletSigner } from '@/utils/contract.signer';
+import useProvider from '@/hooks/useProvider';
 
 interface IProps {
   requestID: string;
@@ -19,11 +22,21 @@ const RequestAccountModal = ({ requestID, request, onClose }: IProps) => {
   const userInfo = useCurrentUserInfo();
   const [loading, setLoading] = React.useState(false);
   const accounts = useAppSelector(listAccountsSelector);
+  const provider = useProvider();
+  const userSecretKey = useUserSecretKey();
 
   const onRejectRequest = async () => {
-    if (!requestID || !userInfo) return;
+    if (!requestID || !userInfo || !userSecretKey || !provider) return;
+    if (userInfo.address.toLowerCase() !== userSecretKey.address.toLowerCase()) {
+      return;
+    }
     setLoading(true);
     const connector = getConnector(requestID);
+    let signature = '';
+    if (request.signMessage) {
+      const walletSigner = getWalletSigner(userSecretKey.privateKey, provider);
+      signature = await walletSigner.signMessage(request.signMessage);
+    }
     try {
       await connector.postResultAccount({
         btcAddress: '',
@@ -31,6 +44,7 @@ const RequestAccountModal = ({ requestID, request, onClose }: IProps) => {
         isCancel: true,
         method: TC_CONNECT.RequestMethod.account,
         accounts: [],
+        signature: signature,
       });
     } catch (e) {
       setLoading(false);
@@ -56,7 +70,12 @@ const RequestAccountModal = ({ requestID, request, onClose }: IProps) => {
   };
 
   return (
-    <BaseModal show={!!requestID} handleClose={onRejectRequest} title="Connect" width={600}>
+    <SignerModal
+      show={!!requestID}
+      onClose={onRejectRequest}
+      title={request.signMessage ? 'Connect and Signature request' : 'Connect'}
+      width={600}
+    >
       <Container>
         <Text size="body-large">
           Approve this request to prove you have access to this wallet and you can start to use{' '}
@@ -65,16 +84,35 @@ const RequestAccountModal = ({ requestID, request, onClose }: IProps) => {
           </a>
           .
         </Text>
+        <SelectAccount
+          title="Account"
+          className="mt-16 mb-16"
+          setLoading={isLoading => {
+            console.log(isLoading);
+          }}
+        />
+        <Divider className="mb-24 mt-24" />
+        <AdvanceWrapper className="mt-24">
+          <Text size="body" color="text-primary" align="center" className="mb-16 mt-16">
+            You only sign this message if you fully understand the content and trust the requesting site.
+          </Text>
+          <Text size="body" color="text-secondary">
+            You are signing:
+          </Text>
+          <div className="box">
+            <Text size="body">{request.signMessage}</Text>
+          </div>
+        </AdvanceWrapper>
         <ButtonGroup className="mt-32">
           <Button disabled={loading} variants="outline" sizes="stretch" onClick={onRejectRequest}>
-            Cancel
+            Reject
           </Button>
           <Button disabled={loading} sizes="stretch" onClick={onAcceptRequest}>
             Sure
           </Button>
         </ButtonGroup>
       </Container>
-    </BaseModal>
+    </SignerModal>
   );
 };
 
