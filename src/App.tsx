f7ab677759dcda1da035eac65cfb41cacf338832
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-25 18:13:56 +0700

    chore: toast error

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ const App: React.FC = (): React.ReactElement => {
             reverseOrder={false}
             toastOptions={{
               style: {
-                maxWidth: '550px',
+                lineBreak: 'anywhere',
               },
             }}
           />
