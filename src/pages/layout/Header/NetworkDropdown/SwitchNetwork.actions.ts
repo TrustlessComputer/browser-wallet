407f26bbd241b7fe54b1e049b335ed272dba6cd2
SangNX <130561684+camewell071@users.noreply.github.com>
2023-05-25 13:49:20 +0700

    chore: add loading provider

--- src/pages/layout/Header/NetworkDropdown/SwitchNetwork.actions.ts
+++ src/pages/layout/Header/NetworkDropdown/SwitchNetwork.actions.ts
@@ -2,9 +2,11 @@ import networkIns, { INetwork } from '@/lib/network.helpers';
 import sleep from '@/utils/sleep';
 import toast from 'react-hot-toast';
 import { setIsLockedWallet } from '@/state/wallet/reducer';
+import { ILoadingPayload } from '@/contexts/loader.context';
 
 interface IComponent {
   onPreloader: () => void;
+  setLoading: (payload: ILoadingPayload) => void;
 }
 
 export interface ISwitchNetworkAction {
@@ -22,12 +24,15 @@ export class SwitchNetworkAction implements ISwitchNetworkAction {
 
   switchNetwork = async (network: INetwork) => {
     try {
+      this.component.setLoading({ isLoading: true });
       networkIns.switchNetwork(network);
       this.dispatch(setIsLockedWallet(true));
-      await sleep(0.2);
+      await sleep(0.5);
       window.location.reload();
     } catch (e) {
       toast.error('Can not switch network.');
+    } finally {
+      this.component.setLoading({ isLoading: false });
     }
   };
 }
